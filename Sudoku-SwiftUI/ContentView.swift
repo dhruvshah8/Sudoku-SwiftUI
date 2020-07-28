70d93748a55024fa77b79ef472b5afbf1f7356ff
dhruvshah8 <dhruvshah5784@gmail.com>
2020-07-28 17:17:10 -0400

    Initial Commit

---
+++ Sudoku-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sudoku-SwiftUI
+//
+//  Created by Dhruv Shah on 2020-07-28.
+//  Copyright © 2020 Dhruv Shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
